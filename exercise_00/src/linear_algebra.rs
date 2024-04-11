8c0654f0ad87e928f5b489e64f041e2701a3a7af
faventur <faventur@student.42mulhouse.fr>
2024-04-11 16:22:11 +0200

    created clsses using vectors and array passed by reference

--- exercise_00/src/linear_algebra.rs
+++ exercise_00/src/linear_algebra.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 #[derive(Debug, PartialEq)]
 pub struct Vector<K> {
 	values: Vec<K>,
@@ -11,12 +13,27 @@ pub struct Matrix<K> {
 	rows: usize
 }
 
-impl<K: Clone> Vector<K> {
+impl<K> fmt::Display for Vector<K>
+where K: fmt::Display {
+	fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+		write!(fmt, "[")?;
+		for (i, n) in self.values.iter().enumerate() {
+			write!(fmt, "{}", n)?;
+			if i < self.values.len() - 1 {
+				write!(fmt, " ")?;
+			}
+		}
+		write!(fmt, "]")?;
+		Ok(())
+	}
+}
+
+impl<K: Clone + std::fmt::Display> Vector<K> {
 	pub fn new(arr: &[K]) -> Self {
-	   Vector {
+		Vector {
 			values: arr.to_vec(),
 			rows: arr.len()
-	   }
+		}
 	}
 
 	pub fn from(arr: &[K]) -> Self {
@@ -24,5 +41,61 @@ impl<K: Clone> Vector<K> {
 			values: arr.to_vec(),
 			rows: arr.len()
 		}
+	}
+
+	pub fn print(&self) {
+		println!("{}", self);
+	}
+}
+
+impl<K> fmt::Display for Matrix<K>
+where K: fmt::Display {
+	fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+		write!(fmt, "[")?;
+		for (i, v) in self.values.iter().enumerate() {
+			write!(fmt, "[")?;
+			for (j, n) in v.iter().enumerate() {
+				write!(fmt, "{}", n)?;
+				if j < v.len() - 1 {
+					write!(fmt, " ")?;
+				}
+			}
+			write!(fmt, "]")?;
+			if i < self.values.len() - 1 {
+				write!(fmt, "\n")?;
+			}
+		}
+		write!(fmt, "]")?;
+		Ok(())
+	}
+}
+
+impl<K: Clone + std::fmt::Display> Matrix<K> {
+	pub fn new(arr: &[&[K]]) -> Self {
+		let mut ret = Vec::with_capacity(arr.len());
+		for v in arr.iter() {
+			ret.push(v.to_vec());
+		}
+		Matrix {
+			values: ret,
+			rows: arr.len(),
+			cols: arr[0].len()
+		}
+	}
+
+	pub fn from(arr: &[&[K]]) -> Self {
+		let mut ret = Vec::with_capacity(arr.len());
+		for v in arr.iter() {
+			ret.push(v.to_vec());
+		}
+		Matrix {
+			values: ret,
+			rows: arr.len(),
+			cols: arr[0].len()
+		}
+	}
+
+	pub fn print(&self) {
+		println!("{}", self);
 	}
 }
--- exercise_00/src/main.rs
+++ exercise_00/src/main.rs
@@ -1,11 +1,14 @@
 // use std::panic;
 use crate::linear_algebra::Vector;
-// use crate::linear_algebra::Matrix;
+use crate::linear_algebra::Matrix;
 
 pub mod linear_algebra;
  
 fn main() {
-	let mut u = Vector::from(&[2., 3.]);
+	let u = Vector::from(&[2., 3.]);
+	u.print();
 	let v = Vector::from(&[5., 7.]);
+	v.print();
+	let x = Matrix::from(&[&[5., 7.], &[3., 4.]]);
+	x.print();
 }
-
