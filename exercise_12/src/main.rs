f03dfb5d8791024271c48ad6def1f91fcd53b9fd
faventur <faventur@student.42mulhouse.fr>
2024-06-14 15:05:20 +0200

    used mul_add function in exercise 11: determinant

---
+++ exercise_12/src/main.rs
@@ -0,0 +1,52 @@
+use std::panic;
+use crate::linear_algebra::matrix::Matrix;
+
+pub mod linear_algebra;
+
+fn main() {
+	println!("\n\x1b[31;1;4m   o  .  -  Matrices  -  .  o\x1b[0m\n");
+
+	println!("\n\x1b[32mThe Determinant:\x1b[0m\n");
+
+
+	let _ = panic::catch_unwind(|| {
+		// let z: Matrix<f32> = Matrix::from(&[&[1., 0.,], &[0., 1.]]);
+		// println!("\nz is equal to:\n{}", z);
+
+		// println!("{}", z.determinant());
+		// z.print();
+
+
+		// let u = Matrix::from(&[&[1., 2.,], &[3., 4.]]);
+		// println!("\nu is equal to:\n{}", u);
+
+		// println!("{}", u.determinant());
+		// u.print();
+
+
+
+		let v = Matrix::from(&[&[5., 7., 15.], &[3., 4., 5.], &[5., 7., 15.]]);
+		println!("\nv is equal to:\n{}", v);
+
+		println!("{}", v.determinant());
+		// 0
+		v.print();
+
+
+		let r = Matrix::from(&[&[5., 7., 15., 18.], &[3., 4., 5., 3.], &[5., 7., 15., 3.], &[1., 2., 1., 3.]]);
+		println!("\nv is equal to:\n{}", v);
+
+		println!("{}", r.determinant());
+		// 210
+		r.print();
+
+
+		let s = Matrix::from(&[&[5., 7., 15., 18., 17.], &[3., 4., 5., 3., 3.], &[5., 7., 15., 3., 7.], &[1., 2., 1., 3., 5.], &[5., 7., 15., 18., 7.]]);
+		println!("\nv is equal to:\n{}", v);
+
+		println!("{}", s.determinant());
+		s.print();
+
+	});
+
+}
